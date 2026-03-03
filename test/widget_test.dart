d2a902df37710bb8f5e23832507695576d790cc8
Samia <Samiathaynes@gmail.com>
2026-03-02 20:09:57 -0500

    change: Update widget_test.dart files to reflect new app names

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:act02/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RunMyApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
